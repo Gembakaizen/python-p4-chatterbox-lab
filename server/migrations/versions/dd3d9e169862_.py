bf53393573d120119e06c7640d68201cb818fe4b
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-10-26 20:25:36 +0300

    done

---
+++ server/migrations/versions/dd3d9e169862_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dd3d9e169862
+Revises: 0e12555d4c52
+Create Date: 2023-10-26 17:32:24.416490
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dd3d9e169862'
+down_revision = '0e12555d4c52'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
